6f11d8b2347e22ee2d9d8052fd4447efd49e2ffb
Haziq Hapiz <zyqq.dev@gmail.com>
2022-05-15 01:15:14 +0800

    delete old file

--- src/app/client/OrderCounterApp.java
+++ src/app/client/OrderCounterApp.java
@@ -6,34 +6,45 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
 
+import app.client.thread.ClearScreen;
 import model.*;
 
+import view.OrderCounterView;
+
 /**
- * This class represent the client (Order Counter) app for
- * HornettTeaOrderServerApp
+ * This class represents the client (Cashier) app for the system
  * 
- * Function: to start the client-side program for the application
+ * Function: to start the client-side program for the Cashier
  * 
  * @author DaysonTai
  */
 
-public class OrderCounterApp {
+public class OrderCounterAppTest {
+
+    private static int orderNumber = 1;
+
     public static void main(String args[]) {
 
-        System.out.println("\n\nStarting OrderCashierApp..\n");
+        System.out.println("\n\nStarting OrderCounterApp..\n");
 
         Order order;
         Socket socket;
 
+        String continueOrder = "Yes";
+        Scanner sc = new Scanner(System.in);
+
         List<ItemProduct> itemProducts = new ArrayList<ItemProduct>();
+        List<OrderItem> orderItems = new ArrayList<OrderItem>();
+        OrderCounterView orderCounterView = new OrderCounterView();
 
         try {
-
             // Server information
             int serverPortNo = 8087;
             InetAddress serverAddress = InetAddress.getLocalHost();
@@ -71,91 +82,164 @@ public class OrderCounterApp {
                 System.out.println("\n\t ItemProducts fetched from server \n");
 
             } catch (Exception e) {
+                System.out.println("\n\t Error fetching ItemProducts from server \n");
                 e.printStackTrace();
             }
 
-            int send = 0;
-
+            // Page for making a new order
             do {
+                order = new Order();
+                orderItems.clear();
+                int totalOrderItem = 0;
+                double subTotal = 0;
+                double subTotalAmount = 0;
 
-                Scanner sc = new Scanner(System.in);
-                displayItemProducts(itemProducts);
-                System.out.print("\tSend? >> ");
-                send = sc.nextInt();
+                int choice;
 
-                if (send == 1) {
-                    socket = new Socket(serverAddress, serverPortNo);
+                // Ordering specific items
+                do {
 
-                    order = loadOrder();
+                    ClearScreen.ClearConsole();
 
-                    OutputStream outStream = socket.getOutputStream();
-                    ObjectOutputStream oos = new ObjectOutputStream(outStream);
-                    oos.writeObject(order);
+                    orderCounterView.displayItemProducts(itemProducts);
 
-                    System.out.println("\n\tSending new order to server\n");
-                    socket.close();
-                }
+                    // 2. Select menu and quantity
+                    System.out.println("\n\t Enter 0 to complete this order");
+                    System.out.print("\n\t Choice: ");
+                    choice = sc.nextInt();
 
-                else
-                    break;
+                    // continue to add beverage
+                    if (choice != 0) {
 
-            } while (send == 1);
+                        System.out.print("\t Quantity: ");
+                        int quantity = sc.nextInt();
+                        totalOrderItem = totalOrderItem + quantity;
+
+                        ItemProduct itemProduct = null;
+
+                        for (ItemProduct product : itemProducts) {
+                            if (product.getItemProductId() == choice) {
+                                itemProduct = product;
+                                break;
+                            }
+                        }
+
+                        double itemPrice = itemProduct.getPrice();
+                        subTotalAmount = quantity * itemPrice;
+                        subTotal += subTotalAmount;
+
+                        // 3. Store in ArrayList
+                        OrderItem orderItem = new OrderItem();
+                        orderItem.setItemProduct(itemProduct);
+                        orderItem.setOrderItemId(orderNumber);
+                        orderItem.setOrderStatus("Processing");
+                        orderItem.setQuantity(quantity);
+                        orderItem.setSubTotalAmount(subTotalAmount);
+
+                        // Add orderItem into order
+                        orderItems.add(orderItem);
+                    }
+
+                    // Customer order finish
+                    // 4. Add to Order object
+
+                    int orderId = 2000 + orderNumber;
+
+                    java.util.Date date = new java.util.Date();
+                    Timestamp transactionDate = new Timestamp(date.getTime());
+
+                    order.setSubTotal(subTotal);
+                    order.setTotalOrderItem(totalOrderItem);
+                    order.setOrderItems(orderItems);
+                    order.setOrderId(orderId);
+                    order.setTransactionDate(transactionDate);
+
+                    order = calculateConfirmationOrder(order);
+
+                    // // set Order
+                    // order = setOrder(orderId, orderNumber, transactionDate,
+                    // orderItems, totalOrderItem, subTotal, serviceTax,
+                    // rounding, grandTotal, change);
+
+                    // display the confirmation page
+                    if (choice == 0) {
+                        // clear screen
+                        ClearScreen.ClearConsole();
+
+                        // display confirmation page
+                        orderCounterView.confirmationPage(order);
+
+                        int payChoice = sc.nextInt();
+
+                        // Confirm to pay
+                        if (payChoice == 1) {
+                            System.out.print("\n\tTendered cash: ");
+                            double tenderedCash = sc.nextDouble();
+
+                            order.setTenderedCash(tenderedCash);
+
+                            // 6. Send Order object to server
+                            socket = new Socket(serverAddress, serverPortNo);
+
+                            OutputStream outStream = socket.getOutputStream();
+                            ObjectOutputStream oos = new ObjectOutputStream(outStream);
+                            oos.writeObject(order);
+                            oos.flush();
+
+                            // 7. Receive Order object from server
+                            InputStream inStream = socket.getInputStream();
+                            ObjectInputStream oiStream = new ObjectInputStream(inStream);
+
+                            order = (Order) oiStream.readObject();
+                            socket.close();
+
+                            orderCounterView.displayReceipt(order);
+                        }
+
+                        // Go back to menu
+                        else if (payChoice == 2) {
+                            continue;
+                        }
+                    }
+
+                    sc.nextLine();
+
+                } while (choice != 0);
+
+                // 5. Process Order when customer do payment (initialize all variables using
+                // setters)
+
+                // 7. Print receipt
+                // orderCounterView.displayReceipt(order);
+                sc.nextLine();
+
+                // add 1 to orderNumber
+                orderNumber++;
+
+            } while (true);
 
         } catch (Exception e) {
 
             e.printStackTrace();
         }
+
     }
 
-    // method to create an Order object with dummy data
-    public static Order loadOrder() {
+    public static Order calculateConfirmationOrder(Order order) {
 
-        Order order = new Order();
+        double subTotal = order.getSubTotal();
+        double serviceTax = subTotal * 0.06;
+        double rounded = Math.round(serviceTax * 20.0) / 20.0;
+        double rounding = 0;
+        if (rounded != 0) {
+            rounding = rounded - serviceTax;
+        }
+        double grandTotal = subTotal + serviceTax + rounding;
 
-        order.setOrderId(1);
-        order.setOrderNumber(1001);
-        order.setTransactionDate(new Date());
-
-        ItemProduct itemProduct1 = new ItemProduct();
-        itemProduct1.setItemProductId(1);
-        itemProduct1.setName("Hornett Tea");
-        itemProduct1.setPrice(5.00);
-
-        ItemProduct itemProduct2 = new ItemProduct();
-        itemProduct2.setItemProductId(2);
-        itemProduct2.setName("Hornett Coffee");
-        itemProduct2.setPrice(8.00);
-
-        OrderItem orderItem1 = new OrderItem();
-        orderItem1.setOrderItemId(1);
-        orderItem1.setItemProduct(itemProduct1);
-        orderItem1.setQuantity(2);
-        orderItem1.setSubTotalAmount(10.00);
-
-        OrderItem orderItem2 = new OrderItem();
-        orderItem2.setOrderItemId(2);
-        orderItem2.setItemProduct(itemProduct2);
-        orderItem2.setQuantity(1);
-        orderItem2.setSubTotalAmount(8.00);
-
-        List<OrderItem> orderItems = new ArrayList<OrderItem>();
-        orderItems.add(orderItem1);
-        orderItems.add(orderItem2);
-
-        order.setOrderItems(orderItems);
-        // order.setTotalOrderItem(orderItems.size());
-        order.setTotalOrderItem(3);
-        order.setSubTotal(18.00);
-        order.setServiceTax(1.00);
-        order.setGrandTotal(19.00);
+        order.setServiceTax(serviceTax);
+        order.setRounding(rounding);
+        order.setGrandTotal(grandTotal);
 
         return order;
-    }
-
-    public static void displayItemProducts(List<ItemProduct> itemProducts) {
-        System.out.println("\n\nItem products are: \n");
-        for (ItemProduct itemProduct : itemProducts) {
-            System.out.println(itemProduct.getName() + " " + itemProduct.getPrice());
-        }
     }
 }
