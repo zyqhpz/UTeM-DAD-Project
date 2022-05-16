d15ef03bcc6c0707c4fbe517d8d3e18faf9c3ba2
Haziq Hapiz <zyqq.dev@gmail.com>
2022-05-16 21:38:34 +0800

    fix log

--- src/app/server/thread/LogRecorder.java
+++ src/app/server/thread/LogRecorder.java
@@ -15,26 +15,27 @@ public class LogRecorder {
     /*
      * This method is to record the log of the server to a file.
      * 
-     * @author HaziqHapiz
-     * 
      * @param log
      * 
      */
     public static void recordLog(String log) {
         try {
 
-            File myObj = new File("log.txt");
-            FileWriter myWriter = new FileWriter(myObj, true);
+            // create a file
+            File logFile = new File("log.txt");
+            FileWriter myWriter = new FileWriter(logFile, true);
 
             // get current timestamp in DD/MM/YYYY HH:MM:SS format
             String timestamp = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date());
 
-            if (myObj.createNewFile()) {
-                myWriter.write(timestamp + " - " + log + "\n");
-            } else {
-                myWriter.append(timestamp + " - " + log + "\n");
-            }
+            logFile.createNewFile();
 
+            // check if the log file exists or not and write the log to the file
+            if (logFile.exists()) {
+                myWriter.append(timestamp + " - " + log + "\n");
+            } else {
+                myWriter.write(timestamp + " - " + log + "\n");
+            }
             myWriter.close();
 
         } catch (Exception e) {
